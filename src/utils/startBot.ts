3340bc27ff27b841bfb92e5ef6865040987a1f76
Azizjon <aliqulovazizjon68@gmail.com>
2024-03-11 23:05:49 +0500

    Remove error handling in startBot.ts

--- src/utils/startBot.ts
+++ src/utils/startBot.ts
@@ -2,9 +2,7 @@ const botStart = (bot: any) => {
   bot.launch().then(() => {
     console.log("Aziz");
   });
-  bot.catch((err: any, ctx: any) => {
-    console.log(`Ooops, encountered an error for ${ctx}`, err);
-  });
+
   console.log(`Bot Azizjon has been started...`);
 };
 
