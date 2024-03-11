f02f74b54122538a66c567d3a83dd98b1746436a
Azizjon <aliqulovazizjon68@gmail.com>
2024-03-11 22:56:45 +0500

    Add error handling to bot.catch() in startBot.ts

--- src/utils/startBot.ts
+++ src/utils/startBot.ts
@@ -2,6 +2,9 @@ const botStart = (bot: any) => {
   bot.launch().then(() => {
     console.log("Aziz");
   });
+  bot.catch((err: any, ctx: any) => {
+    console.log(`Ooops, encountered an error for ${ctx}`, err);
+  });
   console.log(`Bot Azizjon has been started...`);
 };
 
